b150de5fedeb1defec216a963d5737d5d40a84a7
milad1372 <miladmomeni1372@yahoo.com>
2023-06-01 04:04:12 -0600

    New artworkSchema Model

--- server/models/Artwork.js
+++ server/models/Artwork.js
@@ -2,10 +2,9 @@ const { Schema } = require('mongoose');
 
 const artworkSchema = new Schema({
   title: {
-    type: String,
+    type: [String],
     required: true,
   },
-  // saved artwork id from Europeana
   artworkId: {
     type: String,
     required: true,
@@ -19,6 +18,191 @@ const artworkSchema = new Schema({
   description: {
     type: String,
     required: true,
+  },
+  completeness: {
+    type: Number,
+  },
+  country: {
+    type: [String],
+  },
+  dataProvider: {
+    type: [String],
+  },
+  dcDescription: {
+    type: [String],
+  },
+  dcDescriptionLangAware: {
+    def: {
+      type: [String],
+    },
+    en: {
+      type: [String],
+    },
+  },
+  dcLanguage: {
+    type: [String],
+  },
+  dcLanguageLangAware: {
+    def: {
+      type: [String],
+    },
+  },
+  dcTitleLangAware: {
+    def: {
+      type: [String],
+    },
+    en: {
+      type: [String],
+    },
+  },
+  edmConcept: {
+    type: [String],
+  },
+  edmConceptLabel: {
+    type: [
+      {
+        def: {
+          type: String,
+        },
+      },
+    ],
+  },
+  edmConceptPrefLabelLangAware: {
+    de: {
+      type: [String],
+    },
+    fi: {
+      type: [String],
+    },
+    ru: {
+      type: [String],
+    },
+    pt: {
+      type: [String],
+    },
+    bg: {
+      type: [String],
+    },
+    lt: {
+      type: [String],
+    },
+    lv: {
+      type: [String],
+    },
+    hr: {
+      type: [String],
+    },
+    fr: {
+      type: [String],
+    },
+    hu: {
+      type: [String],
+    },
+    sk: {
+      type: [String],
+    },
+    sl: {
+      type: [String],
+    },
+    ga: {
+      type: [String],
+    },
+    ca: {
+      type: [String],
+    },
+    sv: {
+      type: [String],
+    },
+    el: {
+      type: [String],
+    },
+    en: {
+      type: [String],
+    },
+    it: {
+      type: [String],
+    },
+    es: {
+      type: [String],
+    },
+    et: {
+      type: [String],
+    },
+    cs: {
+      type: [String],
+    },
+    pl: {
+      type: [String],
+    },
+    ro: {
+      type: [String],
+    },
+    da: {
+      type: [String],
+    },
+    nl: {
+      type: [String],
+    },
+  },
+  edmDatasetName: {
+    type: [String],
+  },
+  edmIsShownAt: {
+    type: [String],
+  },
+  edmPreview: {
+    type: [String],
+  },
+  europeanaCollectionName: {
+    type: [String],
+  },
+  europeanaCompleteness: {
+    type: Number,
+  },
+  guid: {
+    type: String,
+  },
+  id: {
+    type: String,
+  },
+  index: {
+    type: Number,
+  },
+  language: {
+    type: [String],
+  },
+  link: {
+    type: String,
+  },
+  previewNoDistribute: {
+    type: Boolean,
+  },
+  provider: {
+    type: [String],
+  },
+  rights: {
+    type: [String],
+  },
+  score: {
+    type: Number,
+  },
+  timestamp: {
+    type: Date,
+  },
+  timestamp_created: {
+    type: String,
+  },
+  timestamp_created_epoch: {
+    type: Number,
+  },
+  timestamp_update: {
+    type: String,
+  },
+  timestamp_update_epoch: {
+    type: Number,
+  },
+  ugc: {
+    type: [Boolean],
   },
 });
 
